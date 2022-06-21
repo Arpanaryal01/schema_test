6c8c0f0f5c8b5e7dfee1d79dd769def80d699f57
Arpanaryal01 <87761020+Arpanaryal01@users.noreply.github.com>
2022-06-21 14:14:32 +0545

    Update V1.1.1__initial_objects.sql

--- migrations/V1.1.1__initial_objects.sql
+++ migrations/V1.1.1__initial_objects.sql
@@ -2,3 +2,5 @@
 CREATE SCHEMA SCHEMA_CHANGE_TEST;
 
 create or replace sequence SEQ_DICTIONARY_ITEM start = 1 increment by 1;
+
+create or replace sequence SEQ_PERSISTENT_ID start = 20000000000 increment by 1;
